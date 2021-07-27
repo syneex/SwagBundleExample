46a74bdf16dffebe69a94748351b17281e981e67
syneex <sammy@suelzle.eu>
2021-07-27 19:12:14 +0200

    Added first entities

---
+++ src/Core/Content/Bundle/Aggregate/BundleTranslation/BundleTranslationEntity.php
@@ -0,0 +1,72 @@
+<?php declare(strict_types=1);
+
+namespace Swag\BundleExample\Core\Content\Bundle\Aggregate\BundleTranslation;
+
+use Shopware\Core\Framework\DataAbstractionLayer\TranslationEntity;
+use Swag\BundleExample\Core\Content\Bundle\BundleEntity;
+
+class BundleTranslationEntity extends TranslationEntity
+{
+    /**
+     * @var string
+     */
+    protected $bundleId;
+
+    /**
+     * @var string|null
+     */
+    protected $name;
+
+    /**
+     * @var BundleEntity
+     */
+    protected $bundle;
+
+    /**
+     * @return string
+     */
+    public function getBundleId(): string
+    {
+        return $this->bundleId;
+    }
+
+    /**
+     * @param string $bundleId
+     */
+    public function setBundleId(string $bundleId): void
+    {
+        $this->bundleId = $bundleId;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return BundleEntity
+     */
+    public function getBundle(): BundleEntity
+    {
+        return $this->bundle;
+    }
+
+    /**
+     * @param BundleEntity $bundle
+     */
+    public function setBundle(BundleEntity $bundle): void
+    {
+        $this->bundle = $bundle;
+    }
+}
---
+++ src/Core/Content/Bundle/BundleEntity.php
@@ -0,0 +1,116 @@
+<?php declare(strict_types=1);
+
+namespace Swag\BundleExample\Core\Content\Bundle;
+
+use \Swag\BundleExample\Core\Content\Bundle\Aggregate\BundleTranslation;
+use Shopware\Core\Content\Product\ProductCollection;
+use Shopware\Core\Framework\DataAbstractionLayer\Entity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityIdTrait;
+
+class BundleEntity extends Entity
+{
+    use EntityIdTrait;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var string
+     */
+    protected $discountType;
+
+    /**
+     * @var float
+     */
+    protected $discount;
+
+    /**
+     * @var ProductCollection|null
+     */
+    protected $products;
+
+    /**
+     * @var BundleTranslationCollection
+     */
+    protected $translations;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDiscountType(): string
+    {
+        return $this->discountType;
+    }
+
+    /**
+     * @param string $discountType
+     */
+    public function setDiscountType(string $discountType): void
+    {
+        $this->discountType = $discountType;
+    }
+
+    /**
+     * @return float
+     */
+    public function getDiscount(): float
+    {
+        return $this->discount;
+    }
+
+    /**
+     * @param float $discount
+     */
+    public function setDiscount(float $discount): void
+    {
+        $this->discount = $discount;
+    }
+
+    /**
+     * @return ProductCollection
+     */
+    public function getProducts(): ?ProductCollection
+    {
+        return $this->products;
+    }/**
+    * @param ProductCollection $products
+    */
+    public function setProducts(ProductCollection $products): void
+    {
+        $this->products = $products;
+    }
+
+    /**
+     * @return BundleTranslationCollection
+     */
+    public function getTranslations(): BundleTranslationCollection
+    {
+        return $this->translations;
+    }
+
+    /**
+     * @param BundleTranslationCollection $translations
+     */
+    public function setTranslations(BundleTranslationCollection $translations): void
+    {
+        $this->translations = $translations;
+    }
+}
